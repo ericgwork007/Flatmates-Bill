d450dd3570959ec073ef6fa2316e6d2ac43f121d
ericg <ericgwork007@gmail.com>
2025-01-26 15:22:19 -0800

    Added translator

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #When the file turns green after being added to git, which means it is added to the staging area and ready to commit.
 #When we enabled git through VCS -> git, the .git folder got created in the project file directory.
 
+from translate import Translator
 from greets import greetings
 
+translator = Translator(to_lang='pt')
+
 for g in greetings:
-    print(g.title())
+    print(translator.translate(g).title())
