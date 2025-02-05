6cf8f8a8d567f10de8fe1b76b370609d94f0c7b3
ericg <ericgwork007@gmail.com>
2025-02-04 18:11:15 -0800

    Added empty class based on planned design

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-#When the file turns green after being added to git, which means it is added to the staging area and ready to commit.
-#When we enabled git through VCS -> git, the .git folder got created in the project file directory.
+class Bill:
+    """
+    This object called Bill takes as input the amount and
+    period of the bill.
+    """
 
-from translate import Translator
-from greets import greetings
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
 
-translator = Translator(to_lang='pt')
+class Flatmates():
+    """
+    This object type is used to create Flatmate objects
+    which takes as input the names and the no. of days the
+    flatmates lived in the flat, it also calculates the amount
+    each flatmate has to pay using the pays method.
+    """
 
-for g in greetings:
-    print(translator.translate(g).title())
+    def __init__(self, name, no_of_days_stayed):
+        self.name = name
+        self.no_of_days_stayed = no_of_days_stayed
+
+    def pays(self, bill):
+        return bill.amount/2
+
+class GeneratePdf:
+    """
+    This object generates a pdf with the names of the flatmates
+    and the amount each of the flat mate finally has to pay
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
+the_bill = Bill(amount = 1200, period = "March 2024")
+john = Flatmates(name = "John", no_of_days_stayed=15)
+mary = Flatmates(name = "Mary", no_of_days_stayed=20)
+print(john.pays(bill=the_bill))
