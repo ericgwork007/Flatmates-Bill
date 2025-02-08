4079773c928726135753061775a69bfab5b2fd38
ericg <ericgwork007@gmail.com>
2025-02-07 16:08:19 -0800

    Added user input to app.

--- main.py
+++ main.py
@@ -71,11 +71,17 @@ class PdfReport:
 
         webbrowser.open(self.filename)
 
+amt = float(input("Hey user, Enter the Bill amount: "))
+prd = input("Enter the period stayed: ")
+flmt1_name = input("Enter the name of Flatmate1: ")
+flmt1_days_stayed = int(input("Enter the number of days stayed by Flatmate1: "))
+flmt2_name = input("Enter the name of Flatmate2: ")
+flmt2_days_stayed = int(input("Enter the number of days stayed by Flatmate1: "))
 
-the_bill = Bill(amount = 120, period = "April 2024")
-john = Flatmates(name = "John", no_of_days_stayed=15)
-mary = Flatmates(name = "Mary", no_of_days_stayed=20)
-print(john.pays(bill=the_bill.amount, flatmate2=mary))
+the_bill = Bill(amount = amt, period = prd)
+fltmate1 = Flatmates(name = flmt1_name, no_of_days_stayed=flmt1_days_stayed)
+fltmate2 = Flatmates(name = flmt2_name, no_of_days_stayed=flmt2_days_stayed)
+print(fltmate1.pays(bill=the_bill.amount, flatmate2=fltmate2))
 
-pdf_report = PdfReport("Report.pdf")
-pdf_report.generate(flatmate1=john,flatmate2=mary,bill=the_bill)
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1=fltmate1,flatmate2=fltmate2,bill=the_bill)
